16673f2caf8c70fec8e5161993509fe0dfc62d08
kayyal <amir.kayyal@gmail.com>
2019-04-20 17:12:59 +0430

    fix some issue in arrayTrimmer

--- question01/Q01.java
+++ question01/Q01.java
@@ -1,24 +1,38 @@
 package assignment06.question01;
 
+import com.sun.java.swing.action.AlignRightAction;
+
 import java.util.ArrayList;
 
 public class Q01 {
+    public static void main(String[] args) {
+
+        
 
 
-    public ArrayList<Integer> arrayTrimmer (ArrayList<Integer> list){
-        if (list.size()==0)
+    }
+
+
+    public ArrayList<Integer> arrayTrimmer(ArrayList<Integer> list) {
+        if (list.size() == 0)
             return list;
-        if (list.size()%2==1)
-            list.remove(list.size()-1);
+        if (list.size() % 2 == 1)
+            list.remove(list.size() - 1);
 
 
+        for (int i = list.size() - 1; i < 2; i -= 2) {
+            if (i == list.size() - 1) {
+                if (list.get(i - 2) > list.get(0)) {
+                    list.remove(i);
+                    list.remove(i - 1);
+                }
 
-        for (int i = 2; i < list.size()-1; i++) {
-
-            if (list.get(i-1)>list.get(i+2))
-                list.remove(i+1);
-                list.remove(i);
-
+            } else {
+                if (list.get(i - 2) > list.get(i + 1)) {
+                    list.remove(i);
+                    list.remove(i - 1);
+                }
+            }
         }
 
         return list;
