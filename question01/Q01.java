aee6b8dfec4436745cacf3bbbbaa8c3cc9d327e6
kayyal <amir.kayyal@gmail.com>
2019-04-20 17:31:15 +0430

    add a list to test

--- question01/Q01.java
+++ question01/Q01.java
@@ -6,7 +6,26 @@ import java.util.ArrayList;
 public class Q01 {
     public static void main(String[] args) {
 
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(7);
+        list.add(9);
+        list.add(2);
+        list.add(5);
+        list.add(5);
+        list.add(8);
+        list.add(5);
+        list.add(6);
+        list.add(3);
+        list.add(4);
+        list.add(7);
+        list.add(3);
+        list.add(1);
 
+
+        printList(list);
+//        list.remove(4);
+        printList(listTrimmer(list));
 
 
     }
